e0014f429f3c6fc7fe9ca703f146aaf3dfa40296
Justin Ricks <justin.rixx@gmail.com>
2017-01-18 11:40:09 -0700

    Fix a bug causing infinite recursion

--- cell.py
+++ cell.py
@@ -18,6 +18,28 @@ class Cell:
         neighbors = []
 
         # top
+        if self.row - 1 > -1:
+            neighbors.append((self.row - 1, self.col))
+
+        # right
+        if self.col + 1 < len(maze[0]):
+            neighbors.append((self.row, self.col + 1))
+
+        # bottom
+        if self.row + 1 < len(maze):
+            neighbors.append((self.row + 1, self.col))
+
+        # left
+        if self.col - 1 > -1:
+            neighbors.append((self.row, self.col - 1))
+
+        return neighbors
+
+    def get_all_unvisited_neighbors(self, maze):
+        """Get a list of all the neighbors to this cell"""
+        neighbors = []
+
+        # top
         if self.row - 1 > -1 and not maze[self.row - 1][self.col].visited:
             neighbors.append((self.row - 1, self.col))
 
@@ -38,7 +60,7 @@ class Cell:
     def get_neighbor(self, maze):
         """Pick a random neighbor to return"""
 
-        neighbors = self.get_all_neighbors(maze)
+        neighbors = self.get_all_unvisited_neighbors(maze)
 
         if not neighbors:
             return None
